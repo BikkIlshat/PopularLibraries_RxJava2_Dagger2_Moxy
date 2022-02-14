a4e95256c27ccf463856ef7e1382d480ae8f4e5b
BikkIlshat <81274572+BikkIlshat@users.noreply.github.com>
2022-02-14 22:23:25 +0300

    Update MainActivity.kt

--- app/src/main/java/com/hfad/popularlibrariesrxjava2dagger2moxy/framework/activities/MainActivity.kt
+++ app/src/main/java/com/hfad/popularlibrariesrxjava2dagger2moxy/framework/activities/MainActivity.kt
@@ -1,41 +1,45 @@
-package com.hfad.popularlibrariesrxjava2dagger2moxy.framework.activities
+package com.hfad.popularlibrariesrxjava2dagger2moxy
 
+import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.github.terrakok.cicerone.androidx.AppNavigator
-import com.hfad.popularlibrariesrxjava2dagger2moxy.R
-import com.hfad.popularlibrariesrxjava2dagger2moxy.framework.AndroidScreens
-import com.hfad.popularlibrariesrxjava2dagger2moxy.framework.App
-import com.hfad.popularlibrariesrxjava2dagger2moxy.mvp.presenters.BackButtonListener
-import com.hfad.popularlibrariesrxjava2dagger2moxy.mvp.presenters.MainPresenter
-import com.hfad.popularlibrariesrxjava2dagger2moxy.mvp.views.MainView
+import com.hfad.popularlibrariesrxjava2dagger2moxy.App.Navigation.navigatorHolder
+import com.hfad.popularlibrariesrxjava2dagger2moxy.App.Navigation.router
+import com.hfad.popularlibrariesrxjava2dagger2moxy.databinding.ActivityMainBinding
+import com.hfad.popularlibrariesrxjava2dagger2moxy.presentation.MainPresenter
+import com.hfad.popularlibrariesrxjava2dagger2moxy.ui.BackButtonPressed
+import com.hfad.popularlibrariesrxjava2dagger2moxy.views.MainView
 import moxy.MvpAppCompatActivity
 import moxy.ktx.moxyPresenter
 
-class MainActivity : MvpAppCompatActivity(R.layout.activity_main), MainView {
-
-    
+class MainActivity : MvpAppCompatActivity(), MainView {
+    private val binding: ActivityMainBinding by viewBinding(CreateMethod.INFLATE)
     private val navigator = AppNavigator(this, R.id.container)
+    private val mainPresenter by moxyPresenter { MainPresenter(router) }
 
-    private val presenter by moxyPresenter {
-        MainPresenter(App.instance.router, AndroidScreens())
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
     }
-
 
     override fun onResumeFragments() {
         super.onResumeFragments()
-        App.instance.navigatorHolder.setNavigator(navigator)
+        navigatorHolder.setNavigator(navigator)
     }
 
     override fun onPause() {
+        navigatorHolder.removeNavigator()
         super.onPause()
-        App.instance.navigatorHolder.removeNavigator()
     }
 
     override fun onBackPressed() {
         supportFragmentManager.fragments.forEach {
-            if (it is BackButtonListener && it.backPressed()) {
+            if (it is BackButtonPressed && it.backButtonPressed()) {
                 return
             }
+            mainPresenter.back()
         }
-        presenter.backClicked()
     }
+
 }
